b48a19f04d5123818c5e63877c3dec5d13667d29
segevshlomov <segev.junk@gmail.com>
2013-10-30 01:18:05 +0200

    we want generic code!

--- cleanConfigurations.m
+++ cleanConfigurations.m
@@ -4,6 +4,13 @@ function [ cleanConf ] = cleanConfigurations( targets_conf,targets,BEGIN_COL,END
     T=size(targets,1);
     C=size(targets_conf,2); %num of conf
     targets_conf_clean=zeros(1,C);
+
+    BEGIN_COL_details=1;
+    END_COL_details=2;
+    DURATION_COL_details=3;
+    REAL_BEGIN_COL_details=4;
+    REAL_END_COL_details=5;
+
     % run on all conf (num of cols of targets_conf)
     for i=1:C;
         counter=1;
@@ -16,7 +23,7 @@ function [ cleanConf ] = cleanConfigurations( targets_conf,targets,BEGIN_COL,END
         end
         which_targets = which_targets(which_targets > 0);
         targets_options=perms(which_targets);% all permotations of a configuration.
-        targets_options_checked=zeros(1,size(targets_options,1));
+        targets_options_checked=ones(1,size(targets_options,1));
         if (verbose)
             fprintf('cleanConf:  i=%d which_targets=%d\n',i);
             which_targets
@@ -30,34 +37,28 @@ function [ cleanConf ] = cleanConfigurations( targets_conf,targets,BEGIN_COL,END
             b_win_end=0;
             
             option=targets_options(j,:); %the option we are now working on
+            details=zeros(size(option),5); % begin, end, duration, real_begin, real_end
+            %details table will describe for each target the above info%
+
+            for op=1:size(option);
+                details(op,BEGIN_COL_details)=targets(option(op),BEGIN_COL);
+                details(op,END_COL_details)=targets(option(op),END_COL);
+                details(op,DURATION_COL_details)=targets(option(op),DURATION_COL);
+            end
+
+            details(1,REAL_END_COL_details)=details(1,BEGIN_COL_details)+details(1,DURATION_COL_detail)+
             
-            %now checking if option relevant.
-            % "a" represent the first target in the specific order, "b" the
-            %second and "c" the third
-            %"a_win..." represents the end/begin time window
-            a_win_begin=targets(option(1),BEGIN_COL);%begin_column_in_targets = 4
-            a_win_end=targets(option(1),END_COL);%begin_column_in_targets = 4
-            t_a=targets(option(1),DURATION_COL);
-            a_end=t_a+a_win_begin;
-            
-            if size(which_targets,2) > 1
-                b_win_begin=targets(option(2),BEGIN_COL);%begin_column_in_targets = 4
-                b_win_end=targets(option(2),END_COL);%begin_column_in_targets = 4
-                b_begin=max(a_end,b_win_begin);
-                t_b=targets(option(2),DURATION_COL);
-                b_end=b_begin+t_b;
-                    
+            for k=2:max_target_per_drone;
+                if size(which_targets,2) > (k-1))
+                    details(k,REAL_BEGIN_COL_details),= max(details(k-1,REAL_END_COL_details),details(k,BEGIN_COL_details))
+                    details(k,REAL_END_COL_details),= details(k,REAL_BEGIN_COL_details) + details(k,DURATION_COL_details)
+                end
+                if (details(k,REAL_END_COL_details)>details(k,END_COL_details);
+                    targets_options_checked(j)=0;
+                    %can break here.......
+                end
             end
-            if size(which_targets,2) > 2
-                c_win_begin=targets(option(3),BEGIN_COL);%begin_column_in_targets = 4
-                c_win_end=targets(option(3),END_COL);%begin_column_in_targets = 4
-                c_begin=max(b_end,c_win_begin);
-                t_c=targets(option(3),DURATION_COL);
-                c_end=c_begin+t_c;
-            end
-            if ((c_end <= c_win_end) && (b_end <= b_win_end) && (a_end <= a_win_end));
-                targets_options_checked(j)=1; 
-            end
+           
             if (verbose)
                 fprintf('a_end=%d, a_win_end =%d, b_end=%d, b_win_end =%d,c_end=%d, c_win_end =%d\n',a_end,a_win_end,b_end,b_win_end,c_end,c_win_end);
                 fprintf('res = %d,j=%d,i=%d\n',targets_options_checked(j),j,i);
