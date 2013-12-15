410424fd7a87189d6a47b3ac2914c4d3737e473a
ilantch <ilantch@gmail.com>
2013-12-15 21:31:31 +0200

    adding max flight time making code more generic

--- main.m
+++ main.m
@@ -4,20 +4,21 @@ function [lp] = main(file,targets,agent2sensors)
         agent2sensors               = xlsread(file,'agent2sensors');    
     end
         
-    verbose = 1;
+    verbose = 0;
     
     CONF_VAL_COL                    = 5;
     BEGIN_COL                       = 3;
     END_COL                         = 4;
     DURATION_COL                    = 2;
-    maxNumOfTargetInConfiguration   = 3; % as defined (maybe should be input)
+    maxNumOfTargetInConfiguration   = 5; % as defined (maybe should be input)
+    maxFlightTime                   = 24;
     
     numOfTargets                    = size(targets,1);
     agent2targets                   = build_drone_targets( targets,agent2sensors,verbose);
     allConf                         = getAllConfigurations( numOfTargets,maxNumOfTargetInConfiguration,verbose );
     size(allConf)
     verbose && xlswrite('C:\Magneton\temp.xls',allConf','temp');
-    cleanConf                       = cleanConfigurations( allConf,targets,BEGIN_COL,END_COL,DURATION_COL,maxNumOfTargetInConfiguration,1);
+    cleanConf                       = cleanConfigurations( allConf,targets,BEGIN_COL,END_COL,DURATION_COL,maxNumOfTargetInConfiguration,maxFlightTime,verbose);
     verbose && xlswrite('C:\Magneton\temp.xls',cleanConf','temp2');
     confVal                         = targets(:,CONF_VAL_COL)' * cleanConf;
     agent2conf                      = build_agent2conf( cleanConf,agent2targets,verbose);
