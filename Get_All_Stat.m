c17162c8ec1c4c591c6d37f35e82280cf72cb1ee
unknown <segevs@beck-amir-t440p.iem.technion.ac.il>
2014-08-20 14:27:02 +0300

    asd

---
+++ Get_All_Stat.m
@@ -0,0 +1,9 @@
+function [stat_obj] = Get_All_Stat(files,buildAmount,runAmount,write)
+    for k=1:length(files)
+        file=sprintf('%s.xlsx',files{k});
+        [~,~,~, allConfigurations, agent2conf, ~, ~, ~, ~, ~,allStat] = mainBFS(file,buildAmount,runAmount,write);
+        stat_obj.(sprintf('f_%s',files{k})).allStat = allStat;
+        stat_obj.(sprintf('f_%s',files{k})).allConfigurations = allConfigurations;
+        stat_obj.(sprintf('f_%s',files{k})).agent2conf = agent2conf;
+    end
+end
