91fabdb3be5cffa6f793cecd10a23498ee048a23
unknown <segevs@beck-amir-t440p.iem.technion.ac.il>
2014-08-20 14:14:44 +0300

    h_v_s

---
+++ compare_different_H.m
@@ -0,0 +1,18 @@
+function [stat_matrix] = compare_different_H(files,stat_obj)
+    num_of_h=3;
+    stat_matrix=zeros(length(files),num_of_h+1);
+    for k=1:length(files)
+        allStat                                     = stat_obj.(sprintf('f_%s',files{k})).allStat;
+        allConfigurations                       = stat_obj.(sprintf('f_%s',files{k})).allConfigurations;
+        agent2conf                              = stat_obj.(sprintf('f_%s',files{k})).agent2conf;
+        
+        [~,val_h_staff]                         = Hiuristic_staff(agent2conf,allConfigurations);
+        [~,val_best_conf,~,~]               = new_start_point_to_hiuristic_2(agent2conf,allConfigurations);
+        [~,val_best_conf_substraction]   = new_start_point_to_hiuristic_3(agent2conf,allConfigurations);
+        
+        stat_matrix(k,1)                        =  allStat.val;
+        stat_matrix(k,2)                        =  val_best_conf_substraction;
+        stat_matrix(k,3)                        =  val_best_conf.val;
+        stat_matrix(k,4)                        =  val_best_conf_substraction.val;
+    end
+end
