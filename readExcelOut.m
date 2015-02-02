206a89a05ccd7e4c005be34a1bd69c4d065260de
ilantch <ilantch@gmail.com>
2015-02-02 09:03:06 +0200

    still wip

--- readExcelOut.m
+++ readExcelOut.m
@@ -1,9 +1,26 @@
 function [ output_args ] = readExcelOut(excelOut, currTime)
-    for excelLine = 1:size(excelOut,1):
-        currAgent = excelOut(excelLine,1)
-        currTarget = excelOut(excelLine,2)
-        startTime = excelOut(excelLine,4)
-        endTime = excelOut(excelLine,5)
+    agent2lastTargetLoc = {};
+    agent2currTargetLoc = {};
+    for excelLine = 1:size(excelOut,1)
+        currAgent   = excelOut(excelLine,1);
+        currTarget  = excelOut(excelLine,2);
+        startTime   = excelOut(excelLine,4);
+        endTime     = excelOut(excelLine,5);
+        if (currTime <= endTime) && (currTIme >= startTime)
+            agent2currTargetLoc.(sprintf('%d',currAgent)) = currTarget;
+        elseif currTime > endTime
+            if isfield(agent2lastTargetLoc,sprintf('%d',currAgent)) && (endTime > agent2lastTargetLoc.(sprintf('%d',currAgent)).time)
+                agent2lastTargetLoc.(sprintf('%d',currAgent)).time = endTime;
+                agent2lastTargetLoc.(sprintf('%d',currAgent)).target = currTarget;
+            else
+                agent2lastTargetLoc.(sprintf('%d',currAgent)) = {};
+                agent2lastTargetLoc.(sprintf('%d',currAgent)).time = endTime;
+                agent2lastTargetLoc.(sprintf('%d',currAgent)).target = currTarget;
+            end
+                    
+                    
+                
+                
     end
 end
 
